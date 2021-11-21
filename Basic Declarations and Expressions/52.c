e968943e9240c34bd802b7bca9d472abdd4b5e15
Jaume Forner Escrig <jaumef.escrig96@gmail.com>
2021-11-21 17:11:00 +0100

    Modification exercise 52

--- Basic Declarations and Expressions/52.c
+++ Basic Declarations and Expressions/52.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-    int imin, xmin, num;
-    printf("Input the length of the array: \n");
-    scanf("%d",num);
+    int imin, xmin, num = 0;
+    printf("Input the length of the array:");
+    scanf("%d", &num);
+    printf("\n");
     int x[num];
-    for(int i = 0; i < sizeof(x)/sizeof(int); i++){
-        
+    for(int i = 0; i < num; i++){
+        scanf("%d", &x[i]);
+        if (i == 0){
+            imin = i;
+            xmin = x[i];
+        }else {
+            if(x[i] < xmin){
+                imin = i;
+                xmin = x[i];
+            }
+        }
     }
+    printf("Smallest Value:%d\n", xmin);
+    printf("Position of the element:%d\n", imin);
+    return 0;
 }
